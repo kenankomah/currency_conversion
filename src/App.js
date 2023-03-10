4d33295574c1947a2001b89ecb735bc930ed94f9
Kenneth Ankomah <kenankomah@live.co.uk>
2023-03-10 01:08:36 +0000

    added and styled currency table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//began on 09/03/2023
+// import "bootstrap/dist/css/bootstrap.css";
+import CurrencyTable from "./components/currencyTable";
+import "./bootstrap.min.css";
+import "./App.css";
+import "./home.css";
+import "./table.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CurrencyTable />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/currencyTable.js
@@ -0,0 +1,229 @@
+import React from "react";
+// import closeButton from "../assets/circle-empty-remove.png";
+import closeButton from "../assets/remove-circle.svg";
+
+function CurrencyTable() {
+    return (
+        <>
+            <header>
+                <nav className="navbar navbar-default">
+                    <div className="container">
+                        <div className="navbar-header" href="/">
+                            <a className="navbar-brand" href="/">
+                                ReactJS Currency App
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+
+            <center className="ng-scope">
+                <p id="base">Select the base currency</p>
+
+                <button
+                    ng-click="baseSwitch('usd')"
+                    type="button"
+                    className="btn btn-info btn-xs"
+                >
+                    USD
+                </button>
+                <button
+                    ng-click="baseSwitch('gbp')"
+                    type="button"
+                    className="btn btn-info btn-xs"
+                >
+                    GBP
+                </button>
+                <button
+                    ng-click="baseSwitch('eur')"
+                    type="button"
+                    className="btn btn-info btn-xs"
+                >
+                    EUR
+                </button>
+                <button
+                    ng-click="baseSwitch('cad')"
+                    type="button"
+                    className="btn btn-info btn-xs"
+                >
+                    CAD
+                </button>
+            </center>
+            <div className="container">
+                <div
+                    id="container"
+                    className="col-md-6 col-md-offset-3 ng-scope"
+                >
+                    <p id="time">
+                        <b>Data last updated at:</b>{" "}
+                        <i className="ng-binding">Mar 9, 2023 10:29:03 PM</i>{" "}
+                    </p>
+                    <p ng-hide="validity" id="valCheck" className="ng-hide">
+                        Please enter a valid number
+                    </p>
+                    <p className="txt">
+                        Base currency is: <b className="ng-binding">USD</b>{" "}
+                    </p>
+
+                    <input
+                        id="baseNumber"
+                        ng-model="baseAmount"
+                        type="text"
+                        ng-init="baseAmount='1'"
+                        placeholder="Enter a number"
+                        className="ng-pristine ng-untouched ng-valid ng-not-empty"
+                    />
+
+                    <table className="table table-striped">
+                        <thead className="thead-default">
+                            <tr>
+                                <th id="head">Currency</th>
+                                <th id="head1"></th>
+                                <th id="head2">Conversion</th>
+                            </tr>
+                        </thead>
+                        <tbody id="repeat">
+                            <tr>
+                                <td id="size">
+                                    <img
+                                        width="20"
+                                        src="../currencyConverter/images/flags/usd.png"
+                                        alt=""
+                                    />
+                                    &nbsp;
+                                    <span id="currency" className="ng-binding">
+                                        USD - United States Dollar
+                                    </span>
+                                </td>
+                                <td id="size1" className="symbol ng-binding">
+                                    <img height="15" alt="" />$
+                                </td>
+                                <td id="size2" className="ng-binding">
+                                    1.0000
+                                </td>
+                                <td style={{ cursor: "pointer" }} id="last">
+                                    <img
+                                        className="close-button"
+                                        src={closeButton}
+                                        alt=""
+                                    />
+                                </td>
+                            </tr>
+                            <tr className="ng-scope">
+                                <td id="size">
+                                    <img
+                                        width="20"
+                                        src="../currencyConverter/images/flags/eur.png"
+                                        alt=""
+                                    />
+                                    &nbsp;
+                                    <span id="currency" className="ng-binding">
+                                        EUR - Euro
+                                    </span>
+                                </td>
+                                <td id="size1" className="symbol ng-binding">
+                                    <img height="15" alt="" />€
+                                </td>
+                                <td id="size2" className="ng-binding">
+                                    0.9449
+                                </td>
+                                <td style={{ cursor: "pointer" }} id="last">
+                                    <img
+                                        className="close-button"
+                                        src={closeButton}
+                                        alt=""
+                                    />
+                                </td>
+                            </tr>
+                            <tr
+                                ng-repeat="(key, value) in currencies"
+                                ng-if="$index > 0"
+                                className="ng-scope"
+                            >
+                                <td id="size">
+                                    <img
+                                        width="20"
+                                        src="../currencyConverter/images/flags/gbp.png"
+                                        alt=""
+                                    />
+                                    &nbsp;
+                                    <span id="currency" className="ng-binding">
+                                        GBP - British Pound Sterling
+                                    </span>
+                                </td>
+                                <td id="size1" className="symbol ng-binding">
+                                    <img height="15" alt="" />£
+                                </td>
+                                <td id="size2" className="ng-binding">
+                                    0.8387
+                                </td>
+                                <td style={{ cursor: "pointer" }} id="last">
+                                    <img
+                                        className="close-button"
+                                        src={closeButton}
+                                        alt=""
+                                    />
+                                </td>
+                            </tr>
+                            <tr
+                                ng-repeat="(key, value) in currencies"
+                                ng-if="$index > 0"
+                                className="ng-scope"
+                            >
+                                <td id="size">
+                                    <img
+                                        width="20"
+                                        src="../currencyConverter/images/flags/cad.png"
+                                        alt=""
+                                    />
+                                    &nbsp;
+                                    <span id="currency" className="ng-binding">
+                                        CAD - Canadian Dollar
+                                    </span>
+                                </td>
+                                <td id="size1" className="symbol ng-binding">
+                                    <img height="15" alt="" />$
+                                </td>
+                                <td id="size2" className="ng-binding">
+                                    1.3824
+                                </td>
+                                <td
+                                    style={{ cursor: "pointer" }}
+                                    ng-click="remove($index)"
+                                    id="last"
+                                >
+                                    <img
+                                        className="close-button"
+                                        src={closeButton}
+                                        alt=""
+                                    />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p ng-show="listNumCheck" id="limit" className="ng-hide">
+                        {" "}
+                        10 currency max selected
+                    </p>
+
+                    <div id="container1" className="col-md-6 col-md-offset-3">
+                        <center>
+                            <a href="#/currencies-table">
+                                <button
+                                    ng-click="checkList()"
+                                    id="more"
+                                    type="button"
+                                    className="btn btn-primary"
+                                >
+                                    Add more currencies
+                                </button>
+                            </a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CurrencyTable;
